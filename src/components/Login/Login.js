3eb5f6821e8b2cd106df436df2bd803596f8ad7d
Ilyas G <Ilyas_Ganiyev@g3.ca>
2023-03-26 18:34:11 -0500

    after context refactoring

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 import { act } from "react-dom/test-utils";
+import AuthContext from "../../store/auth-context";
+
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT")
     return { value: action.val, isValid: action.val.includes("@") };
@@ -21,6 +23,7 @@ const passwordReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 const Login = (props) => {
+  const ctx = useContext(AuthContext);
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState("");
@@ -70,7 +73,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,11 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
 export const AuthContextProvider = (props) => {
-  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // useEffect runs AFTER all components are rendered
+  //the function in the useEffect runs once when the app first starts, then it will only be executed if dependencies[] have changed
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn" === "1")) setIsLoggedIn(true);
+  }, []);
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 export default AuthContext;
